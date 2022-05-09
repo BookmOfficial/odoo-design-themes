871ee16594dad668033e2d990d7a6b860946388f
Vincent Baeten <vincent@baeten.dev>
2022-05-09 17:44:21 +0200

    Change live test url of all themes
    Jira-issue: ALPHA-95

--- theme_vehicle/__manifest__.py
+++ theme_vehicle/__manifest__.py
@@ -28,7 +28,7 @@
         'homepage': ['s_cover', 's_text_image', 's_image_text', 's_picture', 's_masonry_block', 's_call_to_action'],
     },
     'license': 'LGPL-3',
-    'live_test_url': 'https://theme-vehicle.odoo.com',
+    'live_test_url': 'https://vehicle.themes.bookm.be',
     'assets': {
         'website.assets_editor': [
             'theme_vehicle/static/src/js/tour.js',
--- theme_zap/__manifest__.py
+++ theme_zap/__manifest__.py
@@ -40,7 +40,7 @@
         'homepage': ['s_banner', 's_three_columns', 's_color_blocks_2', 's_features', 's_masonry_block', 's_references'],
     },
     'license': 'LGPL-3',
-    'live_test_url': 'https://theme-zap.odoo.com',
+    'live_test_url': 'https://zap.themes.bookm.be',
     'assets': {
         'website.assets_editor': [
             'theme_zap/static/src/js/tour.js',
